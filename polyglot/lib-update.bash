3f68a4a782126162007da2803328029a6ec55a89
John Grey <johngrey4296@gmail.com>
2025-10-22 20:14:28 +0100

    [add]: add change cmd

--- polyglot/lib-update.bash
+++ polyglot/lib-update.bash
@@ -26,6 +26,9 @@ function update_polyglot () {
 
 # Release --------------------------------------------------
 function run_release () {
+    header "Running $LEVEL Release"
+
+    subhead "Checking for change notifications"
     # one arg: the level of the version to bump
     local LEVEL="${1:-minor}"
     local CURR_VERSION
@@ -33,17 +36,21 @@ function run_release () {
     if [[ -n $(git --no-pager diff) ]]; then
         fail "There are unstaged changes."
     fi
-    towncrier check || fail "There are no fragment changes. Add descriptions of this release."
+    if [[ -n "$TOWNCRIER_CHANGE_DIR" ]]; then
+       [[ -n $(fdfind . "$TOWNCRIER_CHANGE_DIR") ]] || fail "There are no fragment changes. Add descriptions of this release."
+    else
+        towncrier check || fail "There are no fragment changes. Add descriptions of this release."
+    fi
 
-    header "Running $LEVEL Release"
+    subhead "Calculating Version Number"
     CURR_VERSION=$(version version)
     version "$LEVEL" "set" "+"
     NEW_VERSION=$(version version)
     echo "Project Version $CURR_VERSION -> $NEW_VERSION"
 
-    echo "Generating Changelog"
+    subhead "Generating Changelog"
     towncrier build --yes
-    pg_export
+    run_export
 
     git add --all
     git commit -m "[Release]: ${NEW_VERSION}"
@@ -53,6 +60,9 @@ function run_release () {
 function run_export () {
     header "Exporting Polyglot State"
     export_asdf
-    export_tex
+
+    if [[ -e "$POLYGLOT_ROOT/tex.reqs" ]]; then
+        export_tex
+    fi
     echo "done."
 }
