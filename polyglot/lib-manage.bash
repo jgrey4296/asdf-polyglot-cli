f8748f352c744f119ce7235c3a82d7abd60b3ccc
John Grey <johngrey4296@gmail.com>
2025-11-06 00:08:20 +0000

    [stub]: management cmds and polyglot.toml

---
+++ polyglot/lib-manage.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# lib-manage.bash -*- mode: sh -*-
+
+function pg_manageg_init () {
+    echo "TODO: initialise a polyglot.toml file"
+
+}
+
+function pg_manage_locate () {
+    echo "TODO: look up to find a polyglot.toml file"
+
+}
+
+function pg_manage_add () {
+    echo "TODO: add a languages settings files"
+
+}
+
+function pg_manage_remove () {
+    echo "TODO: remove a languages settings files"
+}
+
+function pg_manage_clean () {
+    echo "TODO: remove unused language settings files"
+
+}
+
+function pg_manage_validate () {
+    echo "TODO: validate the polyglot.toml file"
+}
+
+function pg_manage_checklist () {
+    echo "TODO: checklist"
+}
+
+
+function pg_manage_regenerate () {
+    echo "TODO: regenerate"
+
+    # cargo install --list > .cargo.tools
+    # tex.reqs
+    #
+}
