7564a014886bed578c1caed2a3eb27617a047dcf
John Grey <johngrey4296@gmail.com>
2025-12-03 18:32:49 +0000

    [add]: check cmd

--- polyglot/lib/lib-util.bash
+++ polyglot/lib/lib-util.bash
@@ -40,3 +40,12 @@ function is-help-flag () {
     esac
 }
 
+function check-target () {
+    if [[ ! -d "$POLYGLOT_SRC/$1" ]]; then
+        fail "Target Does not exist: $1"
+    fi
+
+    if [[ ! -e "$POLYGLOT_SRC/$1/$2" ]]; then
+        fail "Target does not have a file to run: $2"
+    fi
+}
