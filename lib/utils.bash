f7df3fb957593c344868ee8b32b8f1122e895115
John Grey <johngrey4296@gmail.com>
2025-12-23 16:38:36 +0000

    [chore]:

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,7 +26,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if <YOUR TOOL> is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -38,26 +37,4 @@ function sort_versions() {
 
 function list_all_versions() {
 	echo "$POLYGLOT_VERSION"
-}
-
-function install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="$3"
-
-
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
-
-	(
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
 }
